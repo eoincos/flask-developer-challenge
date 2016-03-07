f0c1b0dda2a502bc7ec52e215e9b5f36c9c9e3cf
Dionysus Blazakis <dion@terbiumlabs.com>
2016-03-07 18:22:47 +0000

    Docstrings for Flake8

--- gistapi/__init__.py
+++ gistapi/__init__.py
@@ -1,1 +1,4 @@
+# coding=utf-8
+"""A module implementing the gistapi HTTP API server via Flask."""
+
 from .gistapi import *
--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -1,38 +1,77 @@
+# coding=utf-8
+"""
+Exposes a simple HTTP API to search a users Gists via a regular expression.
+
+Github provides the Gist service as a pastebin analog for sharing code and
+other develpment artifacts.  See http://gist.github.com for details.  This
+module implements a Flask server exposing two endpoints: a simple ping
+endpoint to verify the server is up and responding and a search endpoint
+providing a search across all public Gists for a given Github account.
+"""
+
 import requests
 from flask import Flask, jsonify, request
+
+
+# *The* app object
 app = Flask(__name__)
 
 
 @app.route("/ping")
 def ping():
+    """Provide a static response to a simple GET request."""
     return "pong"
 
 
 def gists_for_user(username):
+    """Provides the list of gist metadata for a given user.
+
+    This abstracts the /users/:username/gist endpoint from the Github API.
+    See https://developer.github.com/v3/gists/#list-a-users-gists for
+    more information.
+
+    Args:
+        username (string): the user to query gists for
+
+    Returns:
+        The dict parsed from the json response from the Github API.  See
+        the above URL for details of the expected structure.
+    """
     gists_url = 'https://api.github.com/users/{username}/gist'.format(
             username=username)
     response = requests.get(gists_url)
-    # TODO: What failures could happen?
-    # TODO: Paging? How does this work for users with tons of gists?
+    # BONUS: What failures could happen?
+    # BONUS: Paging? How does this work for users with tons of gists?
+
     return response.json()
 
 
 @app.route("/api/v1/search", methods=['POST'])
 def search():
-    post_data = request.get_json()
+    """Provides matches for a single pattern across a single users gists.
 
-    # TODO: Validate the arguments?
+    Pulls down a list of all gists for a given user and then searches
+    each gist for a given regular expression.
+
+    Returns:
+        A Flask Response object of type application/json.  The result
+        object contains the list of matches along with a 'status' key
+        indicating any failure conditions.
+    """
+    post_data = request.get_json()
+    # BONUS: Validate the arguments?
+
     username = post_data['username']
     pattern = post_data['pattern']
 
     result = {}
     gists = gists_for_user(username)
-    # TODO: Handle invalid users?
+    # BONUS: Handle invalid users?
 
     for gist in gists:
-        # TODO: Fetch each gist and check for the pattern
-        # TODO: What about huge gists?
-        # TODO: Can we cache results in a datastore/db?
+        # REQUIRED: Fetch each gist and check for the pattern
+        # BONUS: What about huge gists?
+        # BONUS: Can we cache results in a datastore/db?
         pass
 
     result['status'] = 'success'
