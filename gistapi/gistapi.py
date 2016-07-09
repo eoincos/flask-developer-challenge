e5001a79e9fade67cc1293c2dddff957293f3f0e
Eoin Costelloe <eoincos@gmail.com>
2016-07-09 15:02:24 +0100

    Adding in basic funcitonality for retrieving a users gists from github

--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -10,6 +10,11 @@ providing a search across all public Gists for a given Github account.
 """
 
 import requests
+import json
+import jsonschema
+import re
+import urllib2
+from jsonschema import validate
 from flask import Flask, jsonify, request
 
 
@@ -39,9 +44,18 @@ def gists_for_user(username):
     """
     gists_url = 'https://api.github.com/users/{username}/gists'.format(
             username=username)
-    response = requests.get(gists_url)
+    
+    response = None
+    
+    try:
+        response = requests.get(gists_url)
+    except requests.exceptions.RequestException as e:
+        pass
     # BONUS: What failures could happen?
+    # RequestException will handle these issue
+    # we could also handle other exceptions such as Timeout to retry the request
     # BONUS: Paging? How does this work for users with tons of gists?
+    # tons of gists will nmean the truncated attribute will be set to True
 
     return response.json()
 
@@ -58,26 +72,77 @@ def search():
         object contains the list of matches along with a 'status' key
         indicating any failure conditions.
     """
+    requestSchema = {
+        "type" : "object",
+        "properties" : {
+            "username" : {"type" : "string"},
+            "pattern" : {"type" : "string"},
+         }
+    }
+    
+    status = 'success'
+    
+    matches = []
+    warnings = []
+    
     post_data = request.get_json()
     # BONUS: Validate the arguments?
-
-    username = post_data['username']
-    pattern = post_data['pattern']
-
-    result = {}
-    gists = gists_for_user(username)
-    # BONUS: Handle invalid users?
-
-    for gist in gists:
-        # REQUIRED: Fetch each gist and check for the pattern
-        # BONUS: What about huge gists?
-        # BONUS: Can we cache results in a datastore/db?
-        pass
-
-    result['status'] = 'success'
-    result['username'] = username
-    result['pattern'] = pattern
-    result['matches'] = []
+    # using jsonschema to validate the json is correct
+    # in this case make the status code fail
+    
+    try:
+        validate(post_data, requestSchema)
+    except jsonschema.exceptions.ValidationError as e:
+        status = 'fail_requestJSON'
+    
+    if status == 'success':
+        username = post_data['username']
+        pattern = post_data['pattern']
+        regex_pattern = re.compile(pattern)
+    
+        result = {}
+        gists = gists_for_user(username)
+        # BONUS: Handle invalid users?
+        # invalid users will lead to invalid requests    
+        # in this case make the status code fail
+        
+        if gists is None:
+            status = 'fail_requestGIST'
+        
+        if status == 'success':
+            for gist in gists:
+                # REQUIRED: Fetch each gist and check for the pattern
+                # opens up each file in a gist and checks the pattern against the content
+                # BONUS: What about huge gists?
+                # truncated attribute will be true
+                # in this case we can return warnings
+                # this tells the user we can't go through all the files
+                # BONUS: Can we cache results in a datastore/db?
+                
+                #print json.dumps(gist, indent=4, sort_keys=True)
+                
+                if gist['truncated'] == True:
+                    warnings.append('more than 300 files in gist ' + gist['id'])
+            
+                matchFound = False
+                
+                for currentFile in gist['files']:
+                    for line in urllib2.urlopen(gist['files'][currentFile]['raw_url']):
+                        if re.search(regex_pattern, line) is not None:
+                            matchFound = True
+                
+                if matchFound:
+                    matches.append('https://gist.github.com/' + username + "/" + gist['id'])
+    
+    if status == 'success':
+        result['status'] = status
+        result['username'] = username
+        result['pattern'] = pattern
+        result['matches'] = matches
+        if len(warnings) > 0:
+            result['warnings'] = warnings
+    else:
+        result['status'] = status
 
     return jsonify(result)
 
